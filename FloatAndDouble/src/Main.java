a397ed6e6e81864d36cdfb00b2802d4970b2ecac
Vinson Mach <54409804+VinsonMach@users.noreply.github.com>
2022-08-23 20:20:28 -0500

    Primitive Types - Float & Double
    Also lbs to kg challenge

---
+++ FloatAndDouble/src/Main.java
@@ -0,0 +1,29 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted Kilograms = " + convertedKilograms);
+
+        double pi = 3.1417927d;
+        System.out.println("Pi = " + pi);
+    }
+}
